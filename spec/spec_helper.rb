6f2c10ada5e64ddeef5e67bb2e5efdbea01aa4e0
Morgan Haskel <morgan@puppetlabs.com>
2014-06-26 17:47:30 -0400

    Pulling common configs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,1 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
-
-RSpec.configure do |c|
-  c.treat_symbols_as_metadata_keys_with_true_values = true
-end
-
-shared_examples :compile, :compile => true do
-  it { is_expected.to compile.with_all_deps }
-end
