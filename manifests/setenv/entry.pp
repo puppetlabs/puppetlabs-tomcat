520856e5562a91a6ba2683ab74414d1f43922cf2
Morgan Haskel <morgan@puppetlabs.com>
2014-07-16 17:38:50 -0400

    Forgot to add the new files.

---
+++ manifests/setenv/entry.pp
@@ -0,0 +1,32 @@
+# Definition tomcat::setenv::entry
+#
+# This define adds an entry to the setenv.sh script.
+#
+# Parameters:
+# - $value is the value of the parameter you're setting
+# - $ensure whether the fragment should be present or absent.
+# - $base_path is the path to create the setenv.sh script under. Should be
+#   either $catalina_base/bin or $catalina_home/bin.
+# - $parameter is the parameter you're setting. Defaults to $name.
+# - $quote_char is the optional character to quote the value with.
+define tomcat::setenv::entry (
+  $value,
+  $ensure     = 'present',
+  $base_path  = "${::tomcat::catalina_home}/bin",
+  $parameter  = $name,
+  $quote_char = undef,
+) {
+
+  if ! defined(Concat["${base_path}/setenv.sh"]) {
+    concat { "${base_path}/setenv.sh":
+      owner => $::tomcat::user,
+      group => $::tomcat::group,
+    }
+  }
+
+  concat::fragment { "setenv-${name}":
+    ensure  => $ensure,
+    target  => "${base_path}/setenv.sh",
+    content => inline_template("<%= parameter %>=<%= quote_char %><%= value %><%= quote_char %>")
+  }
+}
