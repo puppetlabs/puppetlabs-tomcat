36c3c67fd84b3836b814423038a58cde8231840e
Colleen Murphy <colleen@puppetlabs.com>
2015-03-19 19:06:08 -0700

    Fix lint errors and ensure lint fails on warnings

--- examples/instance_with_realm.pp
+++ examples/instance_with_realm.pp
@@ -3,16 +3,18 @@ class { 'tomcat': }
 class { 'java': }
 
 tomcat::instance { 'tomcat8':
-  source_url    => 'http://mirror.reverse.net/pub/apache/tomcat/tomcat-8/v8.0.15/bin/apache-tomcat-8.0.15.tar.gz',
+  source_url   => 'http://mirror.reverse.net/pub/apache/tomcat/tomcat-8/v8.0.15/bin/apache-tomcat-8.0.15.tar.gz',
   purge_realms => true,
-}->
+} ->
 
 tomcat::config::server::realm { 'org.apache.catalina.realm.LockOutRealm':
   realm_ensure => 'present',
 } ->
 
 tomcat::config::server::realm { 'org.apache.catalina.realm.JNDIRealm':
-  realm_ensure => 'present',
-  parent_realm => 'org.apache.catalina.realm.LockOutRealm',
-  additional_attributes => { 'connectionURL' => 'ldap://localhost' },
+  realm_ensure          => 'present',
+  parent_realm          => 'org.apache.catalina.realm.LockOutRealm',
+  additional_attributes => {
+    'connectionURL' => 'ldap://localhost'
+  },
 }
