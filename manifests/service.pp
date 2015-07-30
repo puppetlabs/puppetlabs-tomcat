98d92d91656b9e996f18d3f20795c5a571edf429
Schulz, Bodo <schulz.bodo@guj.de>
2015-07-30 11:05:17 +0200

    enable support for an init-script

--- manifests/service.pp
+++ manifests/service.pp
@@ -144,9 +144,40 @@ define tomcat::service (
       }
     }
 
+    case $::osfamily {
+      'RedHat': {
+        $_config_file = "/etc/sysconfig/${_service_name}"
+      }
+      'Debian': {
+        $_config_file = "/etc/default/${_service_name}"
+      }
+      default: {
+        fail('sorry, currently unsuported')
+      }
+    }
+
+    ini_setting { "sysconfig ${_service_name} TOMCAT_USER":
+      ensure            => present,
+      path              => $_config_file,
+      section           => '',
+      key_val_separator => '=',
+      setting           => 'TOMCAT_USER',
+      value             => "${::tomcat::user}",
+      notify            => Service[$_service_name]
+    } ->
+    ini_setting { "sysconfig ${_service_name} CATALINA_HOME":
+      ensure            => present,
+      path              => $_config_file,
+      section           => '',
+      key_val_separator => '=',
+      setting           => 'CATALINA_HOME',
+      value             => "${_catalina_base}",
+      notify            => Service[$_service_name]
+    } ->
     file { '/etc/init.d/tomcat-general':
       ensure    => file,
-      source    => "puppet:///files/${::osfamily}/init-script"
+      mode      => '0755',
+      source    => "puppet:///modules/tomcat/init-script"
     } ->
     file { "/etc/init.d/$_service_name":
       ensure    => link,
