c6d42c40deca6025899df8579dd6cc61a6aac645
David Schmitt <david.schmitt@puppetlabs.com>
2015-08-27 10:03:07 +0100

    (MAINT) enable install options in package resource
    Based on the code by simontrimboy <simon.trimboy@capgemini.com>:
    
      * renamed the option to match the pattern of the other options in the define
      * added README entry
      * added a unit test

--- manifests/instance/package.pp
+++ manifests/instance/package.pp
@@ -5,9 +5,11 @@
 # Parameters:
 # - $package_ensure is the ensure passed to the package resource.
 # - The $package_name you want to install.
+# - $package_options to pass extra options to the package resource.
 define tomcat::instance::package (
-  $package_ensure = 'installed',
-  $package_name = undef,
+  $package_ensure  = 'installed',
+  $package_name    = undef,
+  $package_options = undef,
 ) {
   if $caller_module_name != $module_name {
     fail("Use of private class ${name} by ${caller_module_name}")
@@ -20,7 +22,8 @@ define tomcat::instance::package (
   }
 
   package { $_package_name:
-    ensure => $package_ensure
+    ensure          => $package_ensure,
+    install_options => $package_options,
   }
 
 }
