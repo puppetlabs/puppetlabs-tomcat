08c923843f15d4be0c6134d3565cfd4d0e8d73c3
David Schmitt <david.schmitt@puppet.com>
2016-04-18 23:11:47 +0100

    (maint) update README and test with current URLs

--- spec/acceptance/readme_spec.rb
+++ spec/acceptance/readme_spec.rb
@@ -25,7 +25,7 @@ describe 'README examples', :unless => stop_test do
       pp = <<-EOS
       class{'java':}
       tomcat::install { '/opt/tomcat':
-        source_url => 'http://www-us.apache.org/dist/tomcat/tomcat-7/v7.0.68/bin/apache-tomcat-7.0.68.tar.gz',
+        source_url => 'https://www-us.apache.org/dist/tomcat/tomcat-7/v7.0.69/bin/apache-tomcat-7.0.69.tar.gz',
       }
       tomcat::instance { 'default':
         catalina_home => '/opt/tomcat',
