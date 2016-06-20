7da77bb1d98dc8eb6d4d75852b928f58c79928d2
Iaroslav Pleten <ipleten@users.noreply.github.com>
2016-06-20 17:19:07 +0300

    Removed  tomcat::setenv::entry $ensure as deprecated

--- manifests/setenv/entry.pp
+++ manifests/setenv/entry.pp
@@ -13,7 +13,6 @@
 #   either $catalina_base/bin or $catalina_home/bin.
 define tomcat::setenv::entry (
   $value,
-  $ensure        = 'present',
   $catalina_home = undef,
   $config_file   = undef,
   $param         = $name,
@@ -61,7 +60,6 @@ define tomcat::setenv::entry (
     $_content = inline_template('export <%= @param %>=<%= @_quote_char %><%= Array(@value).join(" ") %><%= @_quote_char %>')
   }
   concat::fragment { "setenv-${name}":
-    ensure  => $ensure,
     target  => $_config_file,
     content => $_content,
     order   => $order,
