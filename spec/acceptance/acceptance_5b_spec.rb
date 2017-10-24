02d9254e896e32578ad0c5983592a80c4556614c
Bryan Belanger <bryan@autostructure.io>
2017-10-24 12:45:24 -0400

    Merge

--- spec/acceptance/acceptance_5b_spec.rb
+++ spec/acceptance/acceptance_5b_spec.rb
@@ -37,6 +37,30 @@ describe 'Test tomcat install custom type', docker: true, unless: stop_test do
     end
   end
 
+  context 'Delete and reinstall' do
+    it 'Should apply the manifest without error' do
+      pp = <<-EOS
+      class { 'java':}
+      class { 'tomcat': catalina_home => '/opt/apache-tomcat40', }
+
+      user { 'tomcat':
+        ensure => present,
+      }
+
+      group { 'tomcat':
+        ensure => present,
+      }
+
+      tomcat_install { '/opt/apache-tomcat40':
+        ensure => present,
+        version => '7.0.82',
+      }
+      EOS
+      shell('rm -rf /opt/apache-tomcat*', acceptable_exit_codes: [0, 1])
+      apply_manifest(pp, catch_failures: true, acceptable_exit_codes: [0, 2])
+    end
+  end
+
   context 'Change to early version' do
     it 'Should apply the manifest without error' do
       pp = <<-EOS
