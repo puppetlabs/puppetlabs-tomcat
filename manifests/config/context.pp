8b4ab972e618f9b13ec74cb8942ab9291c951de8
Pieter Vogelaar <pieter@pietervogelaar.nl>
2018-06-20 16:43:59 +0200

    Configure global resource attributes in Tomcat 8
    http://tomcat.apache.org/migration-8.html, heading "Web application resources"

--- manifests/config/context.pp
+++ manifests/config/context.pp
@@ -4,6 +4,8 @@
 #
 # Parameters
 # @param catalina_base is the base directory for the Tomcat installation.
+# @param resouces_attributes configures global resource settings
+# @param resouces_remove_attributes configures global resource settings to remove
 
 
 
@@ -20,7 +22,32 @@ define tomcat::config::context (
 
   $_watched_resource = 'set Context/WatchedResource/#text "WEB-INF/web.xml"'
 
-  $changes = delete_undef_values([$_watched_resource])
+  # Global resource settings
+  $resources_base_path = 'Context/Resources'
+  if empty($resources_attributes) {
+    $rm_resources = "rm ${resources_base_path}"
+
+    $changes = delete_undef_values(flatten([
+      $_watched_resource,
+      $rm_resources,
+    ]))
+  } else {
+    $set_resources_attributes = suffix(prefix(join_keys_to_values($resources_attributes, " '"),
+        "set ${resources_base_path}/#attribute/"), "'")
+
+    if !empty(any2array($resources_remove_attributes)) {
+      $rm_resources_attributes =
+        prefix(any2array($resources_remove_attributes), "rm ${resources_base_path}/#attribute/")
+    } else {
+      $rm_resources_attributes = undef
+    }
+
+    $changes = delete_undef_values(flatten([
+      $_watched_resource,
+      $set_resources_attributes,
+      $rm_resources_attributes,
+    ]))
+  }
 
   if ! empty($changes) {
     augeas { "context-${_catalina_base}":
