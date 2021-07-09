cfd1e61011736a0a948d649c64b363e3433f0c72
daiana <daiana.mezdrea@yahoo.com>
2021-07-09 12:02:39 +0300

    Fix urls
    Replace deprecated link with the legacy one
    
    Add missing }
    
    Add tomcat recent source
    
    Update spec_helper_acceptance_local.rb
    
    Update spec_helper_acceptance_local.rb
    
    Update spec_helper_acceptance_local.rb

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -59,7 +59,7 @@ end
 
 def latest_tomcat_tarball_url(version)
   require 'net/http'
-  page = Net::HTTP.get(URI("http://tomcat.apache.org/download-#{version}0.cgi"))
+  page = Net::HTTP.get(URI("https://tomcat.apache.org/download-#{version}0.cgi"))
 
   url = ((match = page.match(%r{https?://.*?apache-tomcat-(.{4,9}).tar.gz})) && match[0])
   return url if url
@@ -71,12 +71,11 @@ def latest_tomcat_tarball_url(version)
   "#{mirror_url}/tomcat/tomcat-#{version}/v#{latest_version}/bin/apache-tomcat-#{latest_version}.tar.gz"
 end
 
-latest7 = latest_tomcat_tarball_url('7')
 latest8 = latest_tomcat_tarball_url('8')
 latest9 = latest_tomcat_tarball_url('9')
-
-TOMCAT7_RECENT_VERSION = ENV['TOMCAT7_RECENT_VERSION'] || latest7
-TOMCAT7_RECENT_SOURCE = latest7
+TOMCAT_LEGACY_VERSION = ENV['TOMCAT_LEGACY_VERSION'] || '7.0.85'
+# Please note that these URLs are http and therefore insecure. To remedy this you can change them to https, although some additional work may be required to match the required protocols of the server.
+TOMCAT7_RECENT_SOURCE = "http://archive.apache.org/dist/tomcat/tomcat-7/v#{TOMCAT_LEGACY_VERSION}/bin/apache-tomcat-#{TOMCAT_LEGACY_VERSION}.tar.gz"
 puts "TOMCAT7_RECENT_SOURCE is #{TOMCAT7_RECENT_SOURCE.inspect}"
 
 TOMCAT8_RECENT_VERSION = ENV['TOMCAT8_RECENT_VERSION'] || latest8
@@ -87,10 +86,7 @@ TOMCAT9_RECENT_VERSION = ENV['TOMCAT9_RECENT_VERSION'] || latest9
 TOMCAT9_RECENT_SOURCE = latest9
 puts "TOMCAT9_RECENT_SOURCE is #{TOMCAT9_RECENT_SOURCE.inspect}"
 
-TOMCAT_LEGACY_VERSION = ENV['TOMCAT_LEGACY_VERSION'] || '7.0.85'
-# Please note that these URLs are http and therefore insecure. To remedy this you can change them to https, although some additional work may be required to match the required protocols of the server.
-TOMCAT_LEGACY_SOURCE = "http://archive.apache.org/dist/tomcat/tomcat-7/v#{TOMCAT_LEGACY_VERSION}/bin/apache-tomcat-#{TOMCAT_LEGACY_VERSION}.tar.gz"
-SAMPLE_WAR = 'http://tomcat.apache.org/tomcat-9.0-doc/appdev/sample/sample.war'
+SAMPLE_WAR = 'https://tomcat.apache.org/tomcat-9.0-doc/appdev/sample/sample.war'
 
 confine_8_array = [
   (os[:family].include?('redhat') &&  os[:release].start_with?('5')),
