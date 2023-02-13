346a9baf0b6347d6b81164529eaceed5c27553a2
Peter Jackson <Peter.Jackson@ed.ac.uk>
2023-02-13 11:23:23 +0000

    Update commons-daemon-native version to match Tomcat bundled version

--- spec/acceptance/acceptance_1b_spec.rb
+++ spec/acceptance/acceptance_1b_spec.rb
@@ -27,7 +27,7 @@ describe 'Acceptance case one', unless: stop_test do
     end
   end
 
-  let(:daemon_version) { '1.3.2' }
+  let(:daemon_version) { '1.3.3' }
 
   context 'Initial install Tomcat and verification' do
     it 'applies the manifest without error' do
